76fe01c5a17bd44ca3c22f9a36dff3405385b669
henrychris <64085210+henrychris@users.noreply.github.com>
2024-11-22 22:29:32 +0100

    change base

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
     site: "https://henrychris.github.io",
-	base: "/my-blog",
+	base: "/blog",
     integrations: [mdx(), sitemap()],
 });
